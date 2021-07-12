8a1c82161eff7ef488bd7598b12b7e823a2eb272
diegolins10 <diegofernandeslins@gmail.com>
2021-07-12 17:55:03 -0300

    Add project files.

---
+++ Worker/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Worker.Entities;
+
+namespace Worker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Department dep = new Department();
+
+            dep.
+        }
+    }
+}
