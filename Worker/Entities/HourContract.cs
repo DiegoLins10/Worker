d90d598a00219f37aeeb2ca1099e8160ba6f2aa8
diegolins10 <diegofernandeslins@gmail.com>
2021-07-12 17:59:50 -0300

    Class HourContract

--- Worker/Entities/HourContract.cs
+++ Worker/Entities/HourContract.cs
@@ -1,10 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Worker.Entities
 {
     class HourContract
     {
+        public DateTime Date { get; set; }
+        public double ValuePerHours { get; set; }
+        public int Hours { get; set; }
+
+        public HourContract()
+        {
+        }
+
+        public HourContract(DateTime date, double valuePerHours, int hours)
+        {
+            Date = date;
+            ValuePerHours = valuePerHours;
+            Hours = hours;
+        }
+
+        public double TotalValue()
+        {
+            return Hours * ValuePerHours;
+        }
     }
 }
